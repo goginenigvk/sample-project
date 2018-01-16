60fda68e434106893a850549d8e52e346a96eb7e
gogineni <goginenigvk@gmail.com>
2018-01-15 21:54:05 -0800

    second commit

--- sample-project/src/sampleproject.java
+++ sample-project/src/sampleproject.java
@@ -3,6 +3,7 @@ public class sampleproject {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to webhooks project");
+		System.out.println("welcome to new commit");
 		// TODO Auto-generated method stub
 
 	}
